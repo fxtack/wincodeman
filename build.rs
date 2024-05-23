48c50d442803d90b64d1211c241b1cb60305c56d
fxtack <fxtack@qq.com>
2024-05-24 00:05:41 +0800

    feat: query ntstatus

--- build.rs
+++ build.rs
@@ -1,6 +1,10 @@
 use std::process::Command;
 
 fn main() {
+    if cfg!(target_os = "windows") {
+        embed_resource::compile("./wincodeman.rc");
+    }
+
     // Inject git commit version information.
     let output = Command::new("git").args(&["rev-parse", "HEAD"]).output().unwrap();
     let commit_hash = String::from_utf8(output.stdout).unwrap();
